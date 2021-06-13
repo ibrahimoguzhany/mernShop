f51a1a1942ef80887227c3702ccf265e029e8c5a
ibrahimoguzhany <ibrahimoguzhany@gmail.com>
2021-06-14 00:29:44 +0300

    api slimmed down by adding controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,37 +1,11 @@
 import express from 'express';
-import AsyncHandler from 'express-async-handler';
 const router = express.Router();
-import Product from '../models/productModel.js';
+import {
+  getProductById,
+  getProducts,
+} from '../controllers/productController.js';
 
-//  @desc Fetch all products
-//  @route GET /api/products
-//  @access Public
-router.get(
-  '/',
-  AsyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    // res.status(401);
-    // throw new Error('Not Authorized');
-    res.json(products);
-  })
-);
-
-//  @desc Fetch single product
-//  @route GET /api/product/:id
-//  @access Public
-router.get(
-  '/:id',
-  AsyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-    res.json(product);
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
